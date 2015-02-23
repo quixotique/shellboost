7a43c6c153c59b14d8c8d9f0bc0c0bb850e1f470
Andrew Bettison <andrewb@acres.com.au>
2015-02-24 10:14:15 +1030

    Add libsh/vim.sh with Vim command-line completion

---
+++ libsh/vim.sh
@@ -0,0 +1,58 @@
+# Shell functions for Vim command-line completion
+# vim:sts=3 sw=3 ts=8 et
+# Copyright 2015 Andrew Bettison
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+__shellboost_include libsh/feature.sh || return $?
+__have_completion || return 0
+__shellboost_include libsh/cd.sh || return $?
+
+# Redefine this function to support different development file layouts
+_tags_file() {
+   _nearest_tags_file
+}
+
+_nearest_tags_file() {
+   ( cd_up_until [ -f tags ] && echo "$PWD/tags" )
+}
+
+_vim_search_tags() {
+    local tagsfile pattern limit
+    pattern="$1"
+    tagsfile="$2"
+    limit="${3:-100}"
+    ex -N -u NONE -i NONE -c 'let &tags="'$tagsfile'"' -c 'echo "wah\\n"' -c 'let n=0 | let memo={} | for tag in taglist("^".escape("'$pattern'",".*\\[]")) | let name = tag["name"] | if !has_key(memo, name) | let n+=1 | if n>'"$limit"' | break | endif | echo name | let memo[name]=1 | endif | endfor' -cq |
+        tr -s '\r' '\n' |
+        sed -n '/^[a-zA-Z_]/p'
+}
+
+_complete_vim_bash() {
+    local cur prev
+    COMPREPLY=()
+    cur=${COMP_WORDS[COMP_CWORD]}
+    prev=${COMP_WORDS[COMP_CWORD-1]}
+    case "$prev" in
+    -t)
+        if tags_file="$(_tags_file)" && [ -f "$tags_file" -a -n "$cur" ]; then
+            COMPREPLY=( $(_vim_search_tags "$cur" "$tags_file" ) )
+        fi
+        ;;
+    esac
+}
+
+_complete_vim_excludelist='*.@(o|O|so|SO|so.!(conf)|SO.!(CONF)|a|A|rpm|RPM|deb|DEB|gif|GIF|jp?(e)g|JP?(E)G|mp3|MP3|mp?(e)g|MP?(E)G|avi|AVI|asf|ASF|ogg|OGG|class|CLASS)'
+
+complete -F _complete_vim_bash -f -X "$_complete_vim_excludelist" vi vim gvim rvim view rview rgvim rgview gview
