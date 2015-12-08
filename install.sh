18660e667ad7bc8d3a56ae6163c52737dcea8d92
Andrew Bettison <andrewb@acres.com.au>
2015-12-09 09:49:52 +1030

    Add install.sh script, move .profile etc into env/

---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+usage() {
+    echo "$0 [-f|--force]"
+}
+
+set -e
+
+opt_force=false
+while [ $# -gt 0 ]; do
+    case "$1" in
+    '-?'|-h|--help)
+        usage
+        exit 0
+        ;;
+    -f|--force)
+        opt_force=true
+        shift
+        ;;
+    -*)
+        echo "$0: unsupported option: $1" >&2
+        exit 1
+        ;;
+    *)
+        break
+        ;;
+    esac
+done
+if [ $# -ne 0 ]; then
+    echo "$0: spurious arguments: $*" >&2
+    exit 1
+fi
+
+install() {
+    local src="${1?}"
+    local dst="${2?}"
+    [ -e "$src" ]
+    if $opt_force || [ ! -e "$dst" ]; then
+        echo "link $dst  ->  $src"
+        ln --symbolic --force "$src" "$dst"
+    fi
+}
+
+cd "${HOME?}"
+install etc/shellboost/env/bash_profile .bash_profile
+install etc/shellboost/env/bashrc       .bashrc
+install etc/shellboost/env/hgrc         .hgrc
+install etc/shellboost/env/profile      .profile
+install etc/shellboost/env/setpath      .setpath
+install etc/shellboost/env/vimrc        .vimrc
