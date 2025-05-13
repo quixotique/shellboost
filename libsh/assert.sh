fb1cdc939bb7363bd7640440050a80582c52331b
Andrew Bettison <andrew.bettison@acaciasystems.com.au>
2025-05-13 18:09:26 +0930

    libsh/assert.sh: Add copyright message

--- libsh/assert.sh
+++ libsh/assert.sh
@@ -1,3 +1,21 @@
+# Shell functions for unit tests
+# vim:sts=3 sw=3 ts=8 et
+# Copyright 2015 Andrew Bettison
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 __run() {
    if [ "$1" = '!' ]; then
       shift
@@ -16,5 +34,3 @@ assert() {
       exit 3
    fi
 }
-
-
